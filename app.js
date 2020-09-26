1ea0efc79bcfbe63440fbf9801b0710ad2111da5
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2020-09-26 15:04:25 +0200

    Check for location provided by user

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-geocode('Warsaw', (error, data) => {
-  if (error) {
-    return console.log(error);
-  }
-
-  forecast(data.longitude, data.latitude, (error, forecastData) => {
+if (!process.argv[2]) {
+  console.log('Please enter a location...');
+} else {
+  const location = process.argv[2];
+  geocode(location, (error, data) => {
     if (error) {
       return console.log(error);
     }
-    console.log(data.location);
-    console.log(forecastData);
+
+    forecast(data.longitude, data.latitude, (error, forecastData) => {
+      if (error) {
+        return console.log(error);
+      }
+      console.log(data.location);
+      console.log(forecastData);
+    });
   });
-});
+}
